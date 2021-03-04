e78795c8eea9e8172cbec2f6382a14c35e8d2999
Mario <vcmario3@gmail.com>
2021-03-04 22:52:36 +0100

    Ej1 práctica 3

---
+++ p3/ej1/src/ej1/Ej1.java
@@ -0,0 +1,67 @@
+package ej1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * Escribe un programa que acepte un número entero n, y después una secuencia de
+ * n números enteros. Entonces, el programa calculará su media, mayor, y menor.
+ * Captura las excepciones que se puedan producir, mostrando un mensaje
+ * informativo.
+ *
+ * @author Mario
+ */
+public class Ej1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int num = 0;
+        int[] valores;
+        int mayor;
+        int menor;
+        double media;
+
+        do {
+            System.out.println("¿De cuántos números se va a hacer la media, el mayor y el menor? (mínimo 1)");
+            num = leerEntero("-> ");
+        } while (num < 1);
+
+        valores = new int[num];
+        media = mayor = menor = valores[0] = leerEntero("Valor 1: ");
+        for (int i = 1; i < valores.length; i++) {
+            valores[i] = leerEntero("Valor " + (i + 1) + ": ");
+            media += valores[i];
+            if (valores[i] < menor) {
+                menor = valores[i];
+            }
+            if (valores[i] > mayor) {
+                mayor = valores[i];
+            }
+            media += valores[i];
+        }
+
+        System.out.println("Media: " + media);
+        System.out.println("Mayor: " + mayor);
+        System.out.println("Menor: " + menor);
+    }
+
+    private static int leerEntero(String mensaje) {
+        Scanner entrada = new Scanner(System.in);
+        int toret = 0;
+        boolean seguir = false;
+        do {
+            System.out.print(mensaje);
+            try {
+                toret = Integer.parseInt(entrada.nextLine());
+                seguir = false;
+            } catch (NumberFormatException exc) {
+                System.out.println("Error, asegúrate de estar introduciendo un número entero: " + exc.getMessage());
+                seguir = true;
+            }
+        } while (seguir);
+        return toret;
+    }
+
+}
